7dab5184a61f0653009147c0178f5795904c245f
Dario <rdc.159@hotmail.com>
2019-03-07 13:23:23 -0500

    34
    actualizacion

--- js/validacionjs.js
+++ js/validacionjs.js
@@ -1,6 +1,7 @@
 var propForm={
     entradas:document.querySelectorAll('input .validar'),
-    valoInput:null
+    valoInput:null,
+    expresionRegular
 }
  var metForm={
      inicio:function(){
@@ -14,10 +15,33 @@ var propForm={
         propForm.valoInput=input.target.value;
         if (propForm.valoInput=="") {
             document.querySelector('#'+ input.target.id).style.background='rgba(255,0,0)';
-            document.querySelector('#'+ input.target.id).style.borderBottom='6px solid rgba(255,0,0.586)';
-            document.querySelector("[for="+input.target.id+"].obligatorio");
-            document.querySelector("[for="+input.target.id+"].obligatorio");
+            document.querySelector('#'+ input.target.id).style.borderBottom='6px solid rgba(255,0,.8)';
+            document.querySelector("[for="+input.target.id+"] .obligatorio").style.display='block';
         }
-     }
- }   
+        document.querySelector("[for="+input.target.id+"] .obligatorio").appendChild(document.createElement("div")).setAttribute("class","error");
+    },
+    inpEscribiendo:function(input){
+        var tipo=input.target.type;
+        switch (tipo) {
+            case 'text':
+                console.log('Esto es un tipo text');
+                propForm.expresionRegular= /^[a-zA-Z]+/;
+                validar=valForm.valText(2,6,propForm.valor)
+                break;
+            case 'passsword':
+                console.log('Esto es un tipo password');
+                break;
+            case 'date':
+                console.log('Esto es un tipo date');
+                break;
+            case 'number':
+                console.log('Esto es un tipo number')
+                break;
+            default:
+                break;
+        }
+    }
+}
 metForm.inicio();
+
+//expresiones regulares imei, url, campor solo enteres, campo solo numeros, solo letras en mayusculas
